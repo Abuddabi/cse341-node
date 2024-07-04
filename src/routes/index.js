3e33e1f529cbaa25c37b2c57b73b3bb2f2410e96
Abuddabi <web.abudabi@gmail.com>
2024-07-04 18:39:36 -0500

    added home page

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const auth = require("./auth");
 
+router.get("/", (req, res) => res.send("<a href='/api-docs'>API DOCS</a>"));
+
 router.use("/api-docs", require("./swagger"));
 router.use("/auth", auth.router);
 router.use("/food", require("./food"));
--- src/routes/swagger.js
+++ src/routes/swagger.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const swaggerUi = require("swagger-ui-express");
 let swaggerDocument = require("../config/swagger/output.json");
+delete swaggerDocument.paths["/"];
 delete swaggerDocument.paths["/api-docs/"];
 
 router.use("/", swaggerUi.serve);
